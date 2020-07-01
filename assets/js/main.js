8dba5cf3ef78741e7a86bd6cce32358c231cbeb8
Alejandro Ramírez <alrg1720@gmail.com>
2020-07-01 11:59:54 -0500

    Forma escalable en precios
    Tomando el feedback proporcionado por la comunidad Frontend Mentor
    se uso objetos y los tag label e input para que a tráves de las
    propiedades que poseen como el checkbox y usando eventos en ellos
    logramos cambiar los precios de una manera mas sencilla
    
    Cambios en HTMl,SASS, JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,91 +1,40 @@
-// const element= document.getElementById('toggle')
-// const btn= document.getElementById('btn-active')
-// const btn2=document.getElementById('btn-active2')
-// const btn3=document.getElementById('btn-active3')
-// const pricing1= document.getElementById('cards-items__pricing1')
-// const pricing2= document.getElementById('cards-items__pricing2')
-// const pricing3= document.getElementById('cards-items__pricing3')
-// const show = () => {
-    
-//     const numeroOriginal1= pricing1.textContent
-//     const numeroOriginal2= pricing2.textContent
-//     const numeroOriginal3= pricing3.textContent
-//     let newvalue1=`$199.99`
-//     let newvalue2=`$249.99`
-//     let newvalue3=`$399.99`
-//     element.addEventListener('click',()=>{
-//         if(newvalue1!=pricing1.textContent && newvalue2!=pricing2.textContent && newvalue3!=pricing3.textContent){
-//             pricing1.textContent=newvalue1
-//             pricing2.textContent=newvalue2
-//             pricing3.textContent=newvalue3
-//         }else{
-//             pricing1.textContent=numeroOriginal1
-//             pricing2.textContent=numeroOriginal2
-//             pricing3.textContent=numeroOriginal3
-//         }
-//         element.classList.toggle('move')
-//         btn.classList.toggle('btn-active')
-//         btn2.classList.toggle('btn-active2')
-//         btn3.classList.toggle('btn-active2')
-//     })
-// }
-
-// show()
-
-
 const element = document.getElementById('toggle')
 const elementos= document.querySelectorAll('#btn')
 const precios= document.querySelectorAll('#cards-items__pricing')
-const originalPrecios=Array.from(precios)
-let arrayPrecios=Array.from(precios)
 let arrayNodos= Array.from(elementos)
-let arrayNuePrecios= ['$199.99','$249.99','$399.99']
-// arrayNodos[1].classList.toggle('btn-active')
-
-// console.log(precios.textContent);
+const toggle = document.querySelector('#plansToggle')
 
 
-const show = () => {
-    element.addEventListener('click',()=>{
+const plans = {
+    monthly:{
+        basic: '$19.99',
+        pro:   '$24.99',
+        master:'$39.99'
+    },
+    annually:{
+        basic: '$199.99',
+        pro: '$249.99',
+        master: '$399.99',
+    }
+};
 
-        // arrayPrecios.map(function(arrayPrecios,index) {
-        //     // console.log(arrayPrecios.textContent);
-        //     arrayNuePrecios.map(function(nuevosprecios){
-        //         console.log(arraysPrecios);
-        //         arrayPrecios.textContent=nuevosprecios
-        //     })
-        // })
+const handlePlanChange = e => {
+    const checked = toggle.checked;
+    console.log(checked);
+    const priceElements = document.querySelectorAll(`[data-plan]`);
+  
+    for (const p of priceElements) {
+      const type = checked ? 'monthly' : 'annually';
+      const plan = p.getAttribute('data-plan');
+      p.innerHTML = plans[type][plan];
+    }
+  };
 
-        if (arrayNuePrecios!=arrayPrecios.textContent) {
-            for (let i=0; i<arrayPrecios.length;i++) {
-            // console.log(arrayPrecios[i].textContent);
-             for (let i = 0; i < arrayNuePrecios.length; i++) {
-             if(arrayNuePrecios[i]!=arrayPrecios[i].textContent){ 
-                //  console.log('Entramos');
-                arrayPrecios[i].textContent = arrayNuePrecios[i];
-             }
-             }
-        }
-        }else if(arrayNuePre===originalPrecios.textContent){
-            console.log('Falso')
-        }
-        
-        // for (let i=0; i<arrayPrecios.length;i++) {
-        //     // console.log(arrayPrecios[i].textContent);
-        //      for (let i = 0; i < arrayNuePrecios.length; i++) {
-        //      if(arrayNuePrecios[i]!=arrayPrecios[i].textContent){ 
-        //         //  console.log('Entramos');
-        //         arrayPrecios[i].textContent = arrayNuePrecios[i];
-        //      }else{
-        //         // for (let i = 0; i < originalPrecios.length; i++) {
-                    
-                    
-        //         // }
-        //      }
-        //      }
-        // }
-        console.log(originalPrecios);
-
+  
+  
+  const show = () => {
+      toggle.addEventListener('click',()=>{
+          
         arrayNodos.map(function(arrayNodos,index){
             //Comprobamos que el index sea el botón que será diferente 
             if (index===1) {
@@ -95,7 +44,10 @@ const show = () => {
             }
         })
         element.classList.toggle('move')
+       
     })
 }
 
 show()
+window.addEventListener('load', handlePlanChange);
+toggle.addEventListener('change', handlePlanChange);
